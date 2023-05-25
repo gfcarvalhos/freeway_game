fb4357227d1918384add3ac1f0efc8f33abc6aed
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-05-24 23:53:10 -0300

    Project upgrading

--- imagens.js
+++ imagens.js
@@ -3,11 +3,13 @@ let imagemDoAtor;
 let imagemDoCarro;
 let imagemDoCarro2;
 let imagemDoCarro3;
+let imagemCarros;
 
 function preload(){
-  imagemDaEstrada = loadImage("material/estrada.png")
-  imagemDoAtor = loadImage("material/ator-1.png")
-  imagemDoCarro = loadImage("material/carro-1.png")
-  imagemDoCarro2 = loadImage("material/carro-2.png")
-  imagemDoCarro3 = loadImage("material/carro-3.png")
+  imagemDaEstrada = loadImage("material/estrada.png");
+  imagemDoAtor = loadImage("material/ator-1.png");
+  imagemDoCarro = loadImage("material/carro-1.png");
+  imagemDoCarro2 = loadImage("material/carro-2.png");
+  imagemDoCarro3 = loadImage("material/carro-3.png");
+  imagemCarros = [imagemDoCarro, imagemDoCarro2, imagemDoCarro3];
 }
